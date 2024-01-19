c01b779fd873971bc9fda9ec1429295fb324c65e
bhav380 <bhaweshrathour18@gmail.com>
2024-01-19 15:34:48 +0530

    changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const {setFlash} = require('./config/flashMiddleware');
 
 // ____________________express server___________________
 const app = express();
-const PORT =  processs.env.PORT || 5000;
+const PORT =  process.env.PORT || 5000;
 
 //____________________database configuration__________________________
 const db = require('./config/keys').MongoURI;
